2e6e82514c3d60bed0dbd69a535c2c203bbee456
ka2yuki <mori1103k@gmail.com>
2024-10-18 15:26:44 +0900

    コンポーネント作成：簡易表示まで。

---
+++ app/components/TodoList.tsx
@@ -0,0 +1,12 @@
+export default function List() {
+    return (
+        // dom..
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+            <li className="mb-2">
+                Componentsフォルダからrender中..{" "}
+            </li>
+            <li>todo2</li>
+            <li>etc..</li>
+        </ol>
+    )
+}
