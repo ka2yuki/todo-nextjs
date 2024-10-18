f2a8a918e670a8dc1787ea1ffaa9ca5009be12fb
ka2yuki <mori1103k@gmail.com>
2024-10-18 17:40:42 +0900

    状態管理なしのTodoアプリの関数追加まで。

--- app/components/TodoForm.tsx
+++ app/components/TodoForm.tsx
@@ -1,24 +1,44 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from "react";
+
+interface todos {
+    title: string,
+    complete: boolean
+};
 
 export default function InputForm() {
+    // states
+    let todos: todos[] = [
+        { title: 'todo1', complete: false }
+    ];
+
+    // component state by JavaScript.
+    // let inputValue: string;
+    // const setInputValue = (text: string) => {
+    //     inputValue = text;
+    // };
+
+    // component state by React.
+    const [inputValue, setInputValue] = useState("");
+
+    const addTodo = (title: string) => {
+        const newTodos = [...todos, { title, complete: false }];
+        todos = newTodos;
+        alert('Button type=Submit!🔥');
+    }
+    const handleSubmit = (e: SubmitEvent): void => {
+        e.preventDefault();
+        addTodo(inputValue);
+        setInputValue("");
+    }
+
     return (
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <a
-                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Button
-            </a>
-            <a
-                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Button2
-            </a>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input
+                placeholder="input someting.."
+                onChange={(e) => setInputValue(e.target.value)} />
+            <button type="submit">add</button>
+        </form>
     )
 }
--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,12 +1,48 @@
+'use client';
+
+interface todos {
+    title: string,
+    complete: boolean
+};
+
 export default function List() {
+    // state
+    let todos: todos[] = [
+        { title: 'todo1', complete: false },
+        { title: 'todo2', complete: false },
+        { title: 'todo3', complete: false }
+    ];
+    // functions
+    const completeTodo = (index: number) => {
+        const newTodos = [...todos];
+        newTodos[index].complete = !newTodos[index].complete;
+        todos = newTodos;
+    };
+    const removeTodo = (index: number) => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        todos = newTodos;
+    };
+
     return (
-        // dom..
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-            <li className="mb-2">
-                Componentsフォルダからrender中..{" "}
-            </li>
-            <li>todo2</li>
-            <li>etc..</li>
-        </ol>
+        <table>
+            <tbody>
+                {todos &&
+                    todos.map((todo, i) => (
+                        <tr key={i}>
+                            <th>{todo.title}</th>
+                            <td>
+                                <button onClick={() => completeTodo(i)}>
+                                    {todo.complete ? "完了" : "未完了"}
+                                </button>
+                                <button onClick={() => removeTodo(i)}>
+                                    削除
+                                </button>
+                            </td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
     )
 }
