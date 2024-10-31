4135b0a43a3105cb5b60ae24931658bf39296fc9
ka2yuki <mori1103k@gmail.com>
2024-10-31 17:17:58 +0900

    メモ帳タイトル メモ帳からWebアプリに記録していくための大まかな概要。

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-0 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-3 row-start-2 items-center sm:items-start">
-        <h1 style={h1Style}>My Tasks</h1>
+        <h1 style={h1Style}>English Words for Development</h1>
+        <p>主に開発にてエラー文やコンソール上に表示される文字、ドキュメントなどで遭遇しそうな英単語からなんか似ている英単語までを覚えて行くための個人的なアプリです。</p>
         <RecoilRoot>
           <InputForm />
           <List />
